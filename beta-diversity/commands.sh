808d30f1168ac4d1bc80976da2a974ecb79a4545
johnjohnchase <llcooljohn@gmail.com>
2013-06-03 15:39:32 -0700

    updated to generated personal-id and university data

--- beta-diversity/commands.sh
+++ beta-diversity/commands.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-# QIIME:30d29ee4f6b5075e515373dbb08548a432315fb3
-# SMP:104f2ab1515052775c47b200c839cf28f3d66573
-# generate per body site principal coordinates files
+# QIIME 1.6.0
+# SMP: c5161db845d9265d0d76d055daada75bc7ae4afe
+# map.txt MD5: d8c3a8dc7246b0319c895967a068a485
+# Author: Jai Ram Rideout
 
-rm input_dms.txt
-echo "unweighted_unifrac_dm.forehead_ts_only.txt.gz" >> input_dms.txt
-echo "unweighted_unifrac_dm.gut_ts_only.txt.gz" >> input_dms.txt
-echo "unweighted_unifrac_dm.palm_ts_only.txt.gz" >> input_dms.txt
-echo "unweighted_unifrac_dm.tongue_ts_only.txt.gz" >> input_dms.txt
-echo "weighted_unifrac_dm.forehead_ts_only.txt.gz" >> input_dms.txt
-echo "weighted_unifrac_dm.gut_ts_only.txt.gz" >> input_dms.txt
-echo "weighted_unifrac_dm.palm_ts_only.txt.gz" >> input_dms.txt
-echo "weighted_unifrac_dm.tongue_ts_only.txt.gz" >> input_dms.txt
-
-for i in `cat input_dms.txt`
-    do
-        FILENAME=$(echo $i | sed 's/.gz//')
-        PC_FILENAME=pcoa/$(echo "${FILENAME}" | sed 's/.txt/_pc.txt/')
-        echo "processing ${FILENAME}"
-        echo "result is ${PC_FILENAME}"
-        #continue
-        gunzip -c $i > ${FILENAME}
-        principal_coordinates.py -i ${FILENAME} -o ${PC_FILENAME}
-        gzip -c ${PC_FILENAME} > ${PC_FILENAME}.gz
-    done
+# Generate per-body-site PCoA plots (2D and 3D) using time-series-only data for
+# weighted and unweighted UniFrac. Color by PersonalID.
+MAP=map.txt
+PID=PersonalID
+UNIV=University
+for ZIPFILE in *weighted_unifrac_dm.*_ts_only_pc.txt.gz
+do
+PCFILE=$(echo $ZIPFILE | sed 's/.gz//')
+  OUTDIRBASE1=$(echo $PCFILE | sed 's/_pc.txt//')_personal_id_plots
+  OUTDIRBASE2=$(echo $PCFILE | sed 's/_pc.txt//')_University_plots
+  mkdir $OUTDIRBASE1
+  mkdir $OUTDIRBASE2
+  gunzip -c $ZIPFILE > $PCFILE
+  make_2d_plots.py -i $PCFILE -m $MAP -b $UNIV -o $OUTDIRBASE1/2d_discrete_personal_id
+  make_3d_plots.py -i $PCFILE -m $MAP -b $UNIV -o $OUTDIRBASE1/3d_discrete_personal_id -s scaled,unscaled
+  make_2d_plots.py -i $PCFILE -m $MAP -b $UNIV -o $OUTDIRBASE2/2d_discrete_university
+  make_3d_plots.py -i $PCFILE -m $MAP -b $UNIV -o $OUTDIRBASE2/3d_discrete_university -s scaled,unscaled
+  tar czf $OUTDIRBASE1.tar.gz $OUTDIRBASE1
+  tar czf $OUTDIRBASE2.tar.gz $OUTDIRBASE2
+done
